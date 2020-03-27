656dc4433d08ceb1759ca917b11ba18a583477e8
Aria <ariasadeghi@gmail.com>
2020-03-26 23:11:05 -0700

    added about me section of page with links

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import About from './components/About';
 
 function App() {
   return (
-   <div>Hello</div>
+    <>
+   {/* <Navbar /> */}
+   <About />
+   </>
   );
 }
 
---
+++ client/src/components/About/About.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+
+
+const useStyles = makeStyles((theme) => ({
+
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+}));
+export default function Album() {
+  const classes = useStyles();
+
+  return (
+   <div className={classes.heroContent}>
+   <Container maxWidth="sm">
+     <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+       Aria Sadeghi
+     </Typography>
+     <Typography variant="h5" align="center" color="textSecondary" paragraph>
+       I am a full stack developer, finding efficient and clean solutions to complex problems.
+     </Typography>
+     <div className={classes.heroButtons}>
+       <Grid container spacing={2} justify="center">
+         <Grid item>
+           <Button href='https://github.com/ariasadeghi530' target="_blank" variant="outlined" color="primary">
+             Github
+           </Button>
+         </Grid>
+         <Grid item>
+           <Button href='https://www.linkedin.com/in/ariasadeghi/' target="_blank"  variant="outlined" color="primary">
+             LinkedIn
+           </Button>
+         </Grid>
+         <Grid item>
+           <Button href="https://docs.google.com/document/d/1p_KiLvFHAaG-TBpaxJ6GWEqYH5HGaTStW9dN9KjurPg/edit?usp=sharing" target="_blank" variant="outlined" color="primary">
+             Resume
+           </Button>
+         </Grid>
+       </Grid>
+     </div>
+   </Container>
+ </div>
+    )
+    
+    }
